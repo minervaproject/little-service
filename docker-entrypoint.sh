e2cbbe676a6566dd872b855cfcb383d03aba7078
cheng <ummcheng@gmail.com>
2017-12-05 16:34:31 -0800

    Import ebs_secrets.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,17 @@
 
 source /venv/bin/activate
 
-echo -e "Calling DJANGO_SETTINGS_MODULE=config.environments.local python server/manage.py makemigrations ..."
+echo -e "Calling DJANGO_SETTINGS_MODULE=config.environments.ebs python server/manage.py makemigrations ..."
 DJANGO_SETTINGS_MODULE=config.environments.ebs python server/manage.py makemigrations
 
-echo -e "Calling DJANGO_SETTINGS_MODULE=config.environments.local python server/manage.py migrate ..."
+echo -e "Calling DJANGO_SETTINGS_MODULE=config.environments.ebs python server/manage.py migrate ..."
 DJANGO_SETTINGS_MODULE=config.environments.ebs python server/manage.py migrate
 
 # echo -e "Starting Celery ..."
 # cd server; celery -A server worker -l info &
 # cd ..
 
-echo -e "Calling DJANGO_SETTINGS_MODULE=config.environments.local python server/manage.py runserver 0:80 ..."
+echo -e "Calling DJANGO_SETTINGS_MODULE=config.environments.ebs python server/manage.py runserver 0:80 ..."
 DJANGO_SETTINGS_MODULE=config.environments.ebs python server/manage.py runserver 0:80
 
 # bash
--- server/config/environments/ebs.py
+++ server/config/environments/ebs.py
@@ -1,4 +1,5 @@
 from config.settings import *
+from config.secrets.ebs_secrets import *
 
 ALLOWED_HOSTS = ['little-service.us-west-2.elasticbeanstalk.com']
 
